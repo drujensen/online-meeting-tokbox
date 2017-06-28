88be4e7a6d310d9d45689b38402c9e2d4e0e6e6a
Dru Jensen <drujensen@gmail.com>
2017-06-27 23:39:56 -0700

    working version using tokbox

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,44 @@
-function hasUserMedia() {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-    || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  return !!navigator.getUserMedia;
+// replace these values with those generated in your TokBox Account
+var apiKey = "45902462";
+var sessionId = "2_MX40NTkwMjQ2Mn5-MTQ5ODYzMDg2OTUzN35FcXA1K0xSMWF1SFpwKzRCVmMzR2YyYnJ-fg";
+var token = "T1==cGFydG5lcl9pZD00NTkwMjQ2MiZzaWc9YzAzYWM2ODlhODQ4YWMwM2U0ODdmMWU3NTg2NjQ1YjMyYTEyNmFlODpzZXNzaW9uX2lkPTJfTVg0ME5Ua3dNalEyTW41LU1UUTVPRFl6TURnMk9UVXpOMzVGY1hBMUsweFNNV0YxU0Zwd0t6UkNWbU16UjJZeVluSi1mZyZjcmVhdGVfdGltZT0xNDk4NjMwODg0Jm5vbmNlPTAuMTU1MDAyMzk5MzA2MzUzMTgmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUwMTIyMjg4Mw==";
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
 }
 
-if (hasUserMedia()) {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-    || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+// (optional) add server code here
+initializeSession();
 
-  //get both video and audio streams from user's camera
-  navigator.getUserMedia({ video: true, audio: true }, function (mediaStream) {
-    var localVideo = document.querySelector('.local');
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
 
-    //insert stream into the video tag
-    attachMediaStream(localVideo, mediaStream);
-  }, function (err) {});
+  // Subscribe to a newly created stream
+  session.on('streamCreated', function(event) {
+    session.subscribe(event.stream, 'subscriber', {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    }, handleError);
+  });
 
-}else {
-  alert("Error. WebRTC is not supported!");
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, initialize a publisher and publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
 }
