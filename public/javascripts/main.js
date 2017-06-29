2eea32bb3dfdcf50e9d3144fce8188ea65f4cf2f
Dru Jensen <drujensen@gmail.com>
2017-06-29 10:56:12 -0700

    desktop sharing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,10 @@ function handleError(error) {
   }
 }
 
+function startSharing() {
+  var room = document.getElementById('room');
+  startSession(room.dataset.apiKey, room.dataset.sessionId, room.dataset.token);
+}
 
 function startSession(apiKey, sessionId, token) {
   var session = OT.initSession(apiKey, sessionId);
@@ -18,6 +22,18 @@ function startSession(apiKey, sessionId, token) {
     }, handleError);
   });
 
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, initialize a publisher and publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      videoshare();
+    }
+  });
+}
+
+function videoshare() {
   // Create a publisher
   var publisher = OT.initPublisher('publisher', {
     insertMode: 'append',
@@ -25,18 +41,25 @@ function startSession(apiKey, sessionId, token) {
     height: '100%'
   }, handleError);
 
-  // Connect to the session
-  session.connect(token, function(error) {
-    // If the connection is successful, initialize a publisher and publish to the session
-    if (error) {
-      handleError(error);
-    } else {
-      session.publish(publisher, handleError);
-    }
-  });
+  session.publish(publisher, handleError);
 }
 
-function startSharing() {
-  var room = document.getElementById('room');
-  startSession(room.dataset.apiKey, room.dataset.sessionId, room.dataset.token);
+
+function screenshare() {
+  OT.checkScreenSharingCapability(function(response) {
+    if (!response.supported || response.extensionRegistered === false) {
+      alert('This browser does not support screen sharing.');
+    } else if (response.extensionInstalled === false) {
+      alert('Please install the screen sharing extension and load your app over https.');
+    } else {
+      // Screen sharing is available. Publish the screen.
+      var publisher = OT.initPublisher('publisher', {
+        videoSource: 'screen',
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+      });
+      session.publish(publisher, handleError)
+    }
+  });
 }
